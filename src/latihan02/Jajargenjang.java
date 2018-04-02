a4cae31ea3a8ec22d3523b8afb846b5e3ed098cc
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-04-02 17:43:17 +0800

    Membuat Atribut dan Behavior

--- src/latihan02/Jajargenjang.java
+++ src/latihan02/Jajargenjang.java
@@ -1,5 +1,15 @@
 package latihan02;
 
 public class Jajargenjang {
-    
+        double alas;
+        double tinggi;
+        
+        void cetakinfo(){
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("Alasnya : "+alas);
+            System.out.println("Tingginya : "+tinggi);
+            System.out.println("");
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        }
+       
 }
