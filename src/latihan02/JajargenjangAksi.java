4bc833ba21cf324a7acdcce5f33df8bc30ee554f
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-04-02 17:52:51 +0800

    Mengisi nilai Atribut

--- src/latihan02/JajargenjangAksi.java
+++ src/latihan02/JajargenjangAksi.java
@@ -4,5 +4,7 @@ public class JajargenjangAksi {
     public static void main(String[] args) {
         
         Jajargenjang j1 = new Jajargenjang();
+            j1.alas = 20;
+            j1.tinggi= 12;
     }
 }
